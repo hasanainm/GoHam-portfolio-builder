db1a954bc44d04f8453b831aa65afff3e00107e1
Hasanain Mohammed <hasanain98@gmail.com>
2020-01-06 22:09:18 -0600

    post and get route created for user account

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,33 @@
 var db = require("../models");
 
 module.exports = function(app) {
+// Get route for getting all of our users
+app.get("/api/useraccount", function(req,res){
+  // findAll will give us all the records for the user
+  db.User.findAll({}).then(function(users){
+    res.json(users)
+  })
+})
 
+//POST route for user account creation
+app.post("/api/useraccount", function(req,res){
+
+//create takes an argument of an object descriving the item we want to insert into our table. In this case we must pass in an object with user name, email and pass
+db.User.create({
+  name: req.body.name,
+  email:req.body.email,
+  password:req.body.password,
+  userID:req.body.userID
+}).then(function(results){
+  //we have access to the new User as an arguement inside of the callback function
+  res.json(results)
+})
+.catch(function(err){
+  // when something goes wrong, an error is thrown
+  // catch prevents our node app from crashing
+  res.json(err)
+})
+})
     
 
 };
