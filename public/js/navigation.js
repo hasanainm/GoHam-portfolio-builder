d96c53a21dece0033abf9e12123f940dc05a0fe8
Anam Mohammad <m.anam0603@gmail.com>
2020-01-12 20:00:38 -0600

    getting inputs ready to store in database

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,10 +1,8 @@
-
 // close hamburger menu after click a
 // $(".navigation__nav li a").on("click", function () {
 //     console.log("clicked");
 //     $(this).parent().fadeOut();
 // });
-
 
 // $(document).on("click",".navigation__nav.out",function(e) {
 //     console.log("clicked");
@@ -12,3 +10,18 @@
 //         $(this).collapse("hide");
 //     }
 // });
+
+document.getElementById("profileLink").addEventListener("click", function (event) {
+    console.log("clicked");
+
+    event.preventDefault();
+    $.ajax("/api/useraccount", {
+        type: "GET"
+    }).then(function (data) {
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].userid == Credential.user.uid) {
+                window.location = "/profilePage" + data[i].id
+            }
+        }
+    })
+})
