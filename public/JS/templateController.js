c6c6feeb7f445e6cf35dee53ef5f971e8f72a9b8
Hasanain Mohammed <hasanain98@gmail.com>
2020-01-18 19:24:43 -0600

    fixed errors in console

--- public/JS/templateController.js
+++ public/JS/templateController.js
@@ -2,28 +2,20 @@ $(document).ready(function () {
   auth.onAuthStateChanged(function (user) {
 
     if (user) {
-      //creating a logout button dynamically only when user is logged in
-      console.log("user signed in as " + user.email)
-      var navbutton = $("<li>");
-      navbutton.addClass("navigation__item");
-      var link = $("<a>Logout</a>").attr("href", "/").addClass("navigation__link").attr("id", "logout");
-      link.appendTo(navbutton);
-      $(".navigation__list").append(navbutton)
 
-      // // click function on navigation "portfolio profile" link. Making it easier for the user to navigate. If the user wants to return to the home screen and decides to go back to the portfolio profile page they can do so.
-      // document.getElementById("profileUser").addEventListener("click", function (event) {
-      //     console.log("clicked");
-      //     event.preventDefault();
-      //     $.ajax("/api/useraccount", {
-      //         type: "GET"
-      //     }).then(function (data) {
-      //         for (var i = 0; i < data.length; i++) {
-      //             if (data[i].userid == user.uid) {
-      //                 window.location = "/cmsPage/" + data[i].id
-      //             }
-      //         }
-      //     });
-      // })
+      document.getElementById("dark-template").addEventListener("click", function (event) {
+        console.log("clicked");
+        event.preventDefault();
+        $.ajax("/api/useraccount", {
+          type: "GET"
+        }).then(function (data) {
+          for (var i = 0; i < data.length; i++) {
+            if (data[i].userid == user.uid) {
+              window.location = "/dark/" + data[i].id
+            }
+          }
+        });
+      })
 
       document.getElementById("minty-template").addEventListener("click", function (event) {
         console.log("clicked");
@@ -38,20 +30,12 @@ $(document).ready(function () {
           }
         });
       })
+
     }
 
     else {
       console.log("user logged out")
     }
 
-    $("#logout").on("click", function (event) {
-      auth.signOut().then(function (action) {
-      }).catch(function (error) {
-        console.log(error)
-      });
-    })
   });
 })
-
-
-
