b7011ed31713c161145aba8a06032b800ac82cbd
Hasanain Mohammed <hasanain98@gmail.com>
2020-01-18 18:37:02 -0600

    routes fixed

--- public/JS/fileUpload.js
+++ public/JS/fileUpload.js
@@ -3,14 +3,18 @@ const customBtn = document.getElementById("custom-button");
 const customTxt = document.getElementById("custom-text");
 
 customBtn.addEventListener("click", function() {
+  console.log("clicked")
   realFileBtn.click();
 });
 
 realFileBtn.addEventListener("change", function() {
+  console.log("changed")
   if (realFileBtn.value) {
+    console.log(realFileBtn.value)
     customTxt.innerHTML = realFileBtn.value.match(
       /[\/\\]([\w\d\s\.\-\(\)]+)$/
     )[1];
+    console.log(customTxt)
   } else {
     customTxt.innerHTML = "No file chosen, yet.";
   }
